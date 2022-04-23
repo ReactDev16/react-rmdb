266df0eddce7af303cf0461558fe9b001055fbaa
Ashok <learnreactstack@gmail.com>
2022-04-23 21:40:29 +0530

    feat(HomePage): Created HomePage and added custom hook

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -12,23 +12,28 @@ export const GlobalStyle = createGlobalStyle`
         --fontMed: 1.2rem;
         --fontSmall: 1rem 
     }
+
     * {
         box-sizing:border-box;
         font-family: 'Abel',sans-serif;
     }
+
     body {
         margin: 0;
         padding: 0;
+
         h1 {
             font-size: 2rem;
             font-weight: 600;
             color: var(--white);
         }
+
         h3 {
             font-size: 1.1rem;
             font-weight: 600;
         }
-        p{
+
+        p {
             font-size: 1rem;
             color: var(--white);
         }
